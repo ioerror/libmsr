5999ae021ff673a7d968f3e60602a60040b43170
Jacob Appelbaum <jacob@appelbaum.net>
2009-07-25 13:35:18 +0000

    This is all fucked up, I'll fix it when I actually sleep for more than an hour in a week.

--- makstripe.c
+++ makstripe.c
@@ -52,34 +52,43 @@ mak_read(int fd, uint8_t tracks)
 	int r;
 	int i;
 	char buf[5];
+	char sample_tmp[2];
 	uint16_t sample_count;
 	printf("Attempting to perform a read...\n");
 
 	r = mak_cmd(fd, MAKSTRIPE_READ_CMD, tracks);
 
+	printf("Command sent!\n");
 	/* Ready response is needed here, check for this. */
-	serial_read(fd, buf, strlen(MAKSTRIPE_READ_RESP));
+	serial_read(fd, buf, strlen(MAKSTRIPE_READ_RESP) -1);
 	printf("We got: %s\n", buf);
 	printf("We expected: %s\n", MAKSTRIPE_READ_RESP);
-	if (!memcmp(buf, MAKSTRIPE_READ_RESP, strlen(MAKSTRIPE_READ_RESP)))
+	if (!memcmp(buf, MAKSTRIPE_READ_RESP, strlen(MAKSTRIPE_READ_RESP))) {
 		printf("Doh!\n");
 		return -1;
+	}
 	printf("Please swipe a card for read...\n");
 
+	memset(buf, 0, 3);
 	/* 'RD '<16bits of length data><data samples> */
 	serial_read(fd, buf, 3);
+	printf("We expect: %s\n", MAKSTRIPE_READ_BUF_PREFIX);
+	printf("We expect it to be of len: %d\n", strlen(MAKSTRIPE_READ_BUF_PREFIX));
 	printf("We got: %s\n", buf);
-	if (!memcmp(buf, MAKSTRIPE_READ_BUF_PREFIX, strlen(MAKSTRIPE_READ_BUF_PREFIX)));
+	r = memcmp(buf, MAKSTRIPE_READ_BUF_PREFIX, strlen(MAKSTRIPE_READ_BUF_PREFIX));
+	if (r != 0) {
 		return -1;
+	}
 	serial_read(fd, &sample_count, 2);
 	printf("Sample count appears to be: %d\n", sample_count);
 
 	/* XXX: We currently clobber the data. We don't return it. */
 	for (i = 0; i < sample_count; i++) {
-		printf("%02x %02x \n", buf[0], buf[1]);
-		serial_read(fd, buf, 2);
+		printf("%d %02x %02x\n", i, sample_tmp[0], sample_tmp[1]);
+		serial_read(fd, sample_tmp, 2);
 	}
 
+	printf("In theory, we have dumped the full sample data now...");
 	/* Lets ensure that the sample read went correctly! */
 	serial_read(fd, buf, 5);
 	printf("Sample read returned status: %s\n", buf);
