4bd2f8aa9e6dd8b055919d0cc3a92dc3a2d60850
Jacob Appelbaum <jacob@appelbaum.net>
2009-07-27 06:01:47 +0000

    This adjusts the function prototype for serial_open to match the function.

--- serialio.h
+++ serialio.h
@@ -1,7 +1,7 @@
 
 #ifndef _SERIALIO_H_
 
-extern int serial_open (char *, int *);
+extern int serial_open (char *, int *,  int, speed_t);
 extern int serial_close (int);
 extern int serial_readchar (int, uint8_t *);
 extern int serial_write (int, void *, size_t);
